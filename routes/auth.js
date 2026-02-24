7f8d61bf9f5f947abfd5f7a752820505c3b02191
Natchanon <inonw2549non@email.com>
2026-02-24 23:45:10 +0700

    add logout to routes auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const { register , login,getMe,logout} = require('../controllers/auth');
+const {protect} = require('../middleware/auth');
 
 const router = express.Router();
 
-const {protect} = require('../middleware/auth');
-
-
+//public routes
 router.post('/register', register);
-router.post('/login' , protect,login) ; 
-router.get('/logout', protect,logout);
+router.post('/login' ,login) ; 
+router.get('/logout',logout);
+
 router.get('/me', protect,getMe);
 module.exports = router;
