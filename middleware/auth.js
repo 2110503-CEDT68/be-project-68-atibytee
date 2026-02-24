d9767dd17193124c2effb80f17b3e75b9a02ae3f
Natchanon <inonw2549non@email.com>
2026-02-24 23:43:26 +0700

    fix middle ware

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,7 +8,10 @@ exports.protect = async (req, res, next) => {
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     token = req.headers.authorization.split(' ')[1];
   }
-
+  // check if token is in cookies
+  else if (req.cookies && req.cookies.token) {
+    token = req.cookies.token;
+  }
   // Make sure token exists
   if (!token) {
     return res.status(401).json({ success: false, message: 'Not authorize to access this route' });
