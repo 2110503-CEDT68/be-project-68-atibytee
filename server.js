bb67fa421c180dc91cf9d2dfee008edf53954013
Ayaya <Builds510@gmail.com>
2026-02-25 22:43:03 +0700

    v2.1

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.set('query parser' , 'extended') ;
 app.use(express.json());//translate json
 app.use(cookieParser());
 
-app.use('/api/v1/rooms' , rooms);
+app.use('/api/v1/coworkingspaces' , rooms);
 app.use('/api/v1/auth',auth) ;
-app.use('/api/v1/reserveations' , reserves);
+app.use('/api/v1/reservations' , reserves);
 
 const PORT = process.env.PORT || 5000 ;
 const server = app.listen(PORT , console.log('SERVER IS RUNNING IN ', process.env.NODE_ENV , 'mode on port ', PORT));
